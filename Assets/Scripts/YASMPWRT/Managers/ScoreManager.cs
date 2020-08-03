a5129f9c948904e92294436815fb6f758672ffc7
GolfNorth <golf155@yandex.ru>
2020-08-03 16:10:08 +0500

    Score Manager created

---
+++ Assets/Scripts/YASMPWRT/Managers/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace YASMPWRT.Managers
+{
+    public sealed class ScoreManager : IDisposable
+    {
+        private int _score;
+
+        public int Score => _score;
+
+        public ScoreManager()
+        {
+            Director.Instance.Set(this);
+            
+            _score = PlayerPrefs.GetInt("Score", 0);
+        }
+        
+        public void Dispose()
+        {
+            Director.Instance.Remove(this);
+            
+            PlayerPrefs.SetInt("Score", _score);
+        }
+
+        public void AddScore(int value)
+        {
+            _score += value;
+        }
+
+        public void ResetScore()
+        {
+            _score = 0;
+        }
+    }
+}
