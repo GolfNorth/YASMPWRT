f30eecc76d5f00422dc62f3884c94b921c40c8f6
GolfNorth <golf155@yandex.ru>
2020-08-03 17:30:08 +0500

    Coin logic done

--- Assets/Scripts/YASMPWRT/Controllers/CoinController.cs
+++ Assets/Scripts/YASMPWRT/Controllers/CoinController.cs
@@ -1,21 +1,33 @@
 ﻿using GeekBrainsInternship.Interfaces;
+using YASMPWRT.Managers;
+using YASMPWRT.Views;
 
 namespace YASMPWRT.Controllers
 {
-    public class CoinController : IController<CoinController>
+    public sealed class CoinController : IController<CoinController>
     {
+        private readonly CoinView _view;
+        private readonly LevelManager _levelManager;
+
+        public CoinController(CoinView view)
+        {
+            _view = view;
+            _levelManager = Director.Instance.Get<LevelManager>();
+        }
+        
         public void Dispose()
         {
         }
 
-        public void Disable()
+        public void Collect()
         {
-            
+            _levelManager.CollectCoin();
+            _view.gameObject.SetActive(false);
         }
 
         public void Reset()
         {
-            
+            _view.gameObject.SetActive(true);
         }
     }
 }
--- Assets/Scripts/YASMPWRT/Views/CoinView.cs
+++ Assets/Scripts/YASMPWRT/Views/CoinView.cs
@@ -1,19 +1,20 @@
 ﻿using UnityEngine;
 using YASMPWRT.Controllers;
-using YASMPWRT.Managers;
 
 namespace YASMPWRT.Views
 {
     public class CoinView : BaseView<CoinController>
     {
+        private void Awake()
+        {
+            Controller = new CoinController(this);
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.name != "Player") return;
 
-            var playerController = Director.Instance.Get<PlayerController>();
-            playerController?.GetCoin();
-            
-            gameObject.SetActive(false);
+            Controller.Collect();
         }
     }
 }
